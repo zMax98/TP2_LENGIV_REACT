2da8ffb9e516f9942c1a9f72942390d8c315a63f
maxiecha <maxiecha98@gmail.com>
2025-09-16 17:06:35 -0300

    TP2 - React con Vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ImageUploader from "./components/ImageUploader";
+
+function App() {
+  return (
+    <div>
+      <h1>Trabajo Práctico N°2</h1>
+      <ImageUploader />
+    </div>
+  );
+}
+
+export default App;
