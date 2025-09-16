58290b6a1131bd33e797344402645ac580b4efb4
maxiecha <maxiecha98@gmail.com>
2025-09-16 19:59:50 -0300

    eliminando archivos innecesarios

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import ImageUploader from "./components/ImageUploader";
 
 function App() {
   return (
-    <div>
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: "50px" }}>
       <h1>Trabajo Práctico N°2</h1>
       <ImageUploader />
-    </div>
+    </div> 
   );
 }
 
